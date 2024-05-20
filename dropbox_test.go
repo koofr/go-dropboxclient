9f878beb670dec1932844955fd00c855b0c7b625
Luka Zakrajšek <luka@bancek.net>
2024-05-20 13:06:20 +0200

    Upgrade dependencies and go version

--- dropbox_test.go
+++ dropbox_test.go
@@ -16,7 +16,7 @@ import (
 	"github.com/koofr/go-dropboxclient/mockdropbox"
 	"github.com/koofr/go-ioutils"
 
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
